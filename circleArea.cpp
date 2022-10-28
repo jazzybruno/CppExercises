a49013feaaeb51ec72894faeaa134a0c9710f7f9
jazzybruno <95282931+jazzybruno@users.noreply.github.com>
2022-10-28 12:02:57 +0200

    Function to calculate volume in a cube

--- circleArea.cpp
+++ circleArea.cpp
@@ -15,21 +15,41 @@ float circmfrenceCircle(float radius){
     cout<< circmufrence <<endl;
 }
 
-float cubeVolume(){
-
+float cubeVolume(float side){
+float volume = side * side * side;
+cout<<"The volume of a cube with the side " << side << " is " << volume <<endl;
 }
 
-void ascending(){
+void ascending(int a , int b , int c){
+int max , smallest , middle , largest;
+max = a;
+if(max < b){
+    max = b;
+    if(max < c){
 
+    }else{
+
+    }
+}else{
+    cout<<"Number " << b << " is the smallest";
+    if(max < c){
+
+    }else{
+
+    }
+}
 }
 
 int main(){
-    float  radius;
+    float  radius , side ;
     cout<<"The program to caculate the area and circmufrence of a circle: \n";
     cout<<"Enter the radius of a circle: ";
     cin>>radius;
     circmfrenceCircle(radius);
     areaCircle(radius);
 
+    cout<<"Enter the side of the cube: ";
+    cin>>side;
+    cubeVolume(side);
 
 }
